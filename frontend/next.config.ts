a9498cd257c46b409e053f1dce9044d9e9facb50
AskaSlip <varenaska@gmail.com>
2025-02-26 15:02:37 +0400

    add front

---
+++ frontend/next.config.ts
@@ -0,0 +1,9 @@
+import type { NextConfig } from "next";
+
+
+const nextConfig: NextConfig = {
+    // output: "standalone",
+    // distDir: process.env.BUILD_PATH || ".next",
+};
+
+export default nextConfig;
